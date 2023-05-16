4a528c701938f4a9c515e574539df1ea93fd7ac7
Ivy-aminga <amingaivy@gmail.com>
2023-05-16 11:50:19 +0300

    delivery details

---
+++ delivery.py
@@ -0,0 +1,11 @@
+class Delivery :
+    
+    def __init__(self,customer_name,customer_id,address,delivery):
+        self.customer_name=customer_name
+        self.customer_id=customer_id
+        self.address=address
+        self.delivery=delivery
+    def  delivery_status():
+        self.delivery="Declined"
+    def  location():
+        self.address=address
