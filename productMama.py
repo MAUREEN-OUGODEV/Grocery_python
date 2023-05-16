f2063eb40ab390df3ccdb31ce4fbd72d45ee9a53
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-05-16 10:33:03 +0300

    pushing to productMama.py

---
+++ productMama.py
@@ -0,0 +1,24 @@
+class Product:
+    def __init__(self,product_name,product_price,quantity,category):
+        self.product_name= product_name
+        self.product_price = product_price
+        self.quantity = quantity
+        self.category = category
+    def item_sold(self, already_sold):
+        
+        if already_sold > self.quantity:
+            print("stocks almost over")
+        else:
+            
+            self.quantity -= already_sold
+            print(f"{already_sold} {self.product_price}")
+
+    def adding_stock(self, add_quality):
+        
+        self.quantity += add_quality
+        print(f"{add_quality} {self.product_name}")
+
+    def total_sales(self):
+       
+        sales = self.price * (self.quantity - sales)
+        return sales    
