9459b5d9edbc5e0a8eec9f19b011c8b2a3969a35
MAUREEN-OUGODEV <ougomaureenakinyi@gmail.com>
2023-05-17 16:43:59 +0300

    changing the total sales

--- productMama.py
+++ productMama.py
@@ -1,9 +1,8 @@
 class Product:
-    def __init__(self,product_name,product_price,quantity,category):
+    def __init__(self,product_name,product_price,quantity):
         self.product_name= product_name
         self.product_price = product_price
         self.quantity = quantity
-        self.category = category
     def item_sold(self, already_sold):
         
         if already_sold > self.quantity:
